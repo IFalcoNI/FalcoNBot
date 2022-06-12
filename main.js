30ab82193eae01b559ddd5c69faa60162c222988
FalcoN <48328425+IFalcoNI@users.noreply.github.com>
2022-06-13 02:40:43 +0300

    done

--- main.js
+++ main.js
@@ -69,7 +69,7 @@ Percent of winnings: ${Math.round(
       }
       return bot.sendMessage(chatId, 'Invalid input');
     } catch (error) {
-      bot.sendMessage(chatId, '', '/start');
+      bot.sendMessage(chatId, 'User was already created');
       console.error(error);
     }
   });
