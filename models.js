a18eaef5fe382f23eed06a5457eb77fdfe8e0101
FalcoN <48328425+IFalcoNI@users.noreply.github.com>
2022-06-13 02:24:01 +0300

    done

--- models.js
+++ models.js
@@ -1,7 +1,7 @@
 const sequelize = require('./connectDB');
 const { DataTypes } = require('sequelize');
 
-const User = sequelize.define('user', {
+const User = sequelize.define('users', {
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
